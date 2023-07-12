1c1b72f5ff0aa59ec8af36538c324de4d8ff7336
Graeme Foster <graemefoster@microsoft.com>
2023-07-12 16:41:23 +0800

    Fix json

--- apps/bot-composer/LockedDownBot/BotComposerOpenAi/SuggestFunctionCall/OpenAiSuggestFunctionCall.cs
+++ apps/bot-composer/LockedDownBot/BotComposerOpenAi/SuggestFunctionCall/OpenAiSuggestFunctionCall.cs
@@ -30,7 +30,7 @@ public class OpenAiSuggestFunctionCall : Dialog
 
 ```response
     {
-        parameters: {
+        ""parameters"": {
             ""parameterName"": ""parameterValue""
         }
     }
