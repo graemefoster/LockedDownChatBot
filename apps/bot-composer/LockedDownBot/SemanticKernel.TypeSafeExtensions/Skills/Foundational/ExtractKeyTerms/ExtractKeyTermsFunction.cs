d3eb5244a55ad4ae18564c3ae2fdaf4c04dd3d4d
Graeme Foster <graemefoster@microsoft.com>
2023-08-04 16:55:51 +0800

    Small model tweak

--- apps/bot-composer/LockedDownBot/SemanticKernel.TypeSafeExtensions/Skills/Foundational/ExtractKeyTerms/ExtractKeyTermsFunction.cs
+++ apps/bot-composer/LockedDownBot/SemanticKernel.TypeSafeExtensions/Skills/Foundational/ExtractKeyTerms/ExtractKeyTermsFunction.cs
@@ -14,7 +14,7 @@ public static class ExtractKeyTermsFunction
         public override string Prompt => """
 {{$Context}}
 
-Given the user input, extract the key terms as a list of words from it
+Given the user input, extract the key terms separated by a newline from it.
 
 --- USER INPUT FOLLOWS
 {{$UserInput}}
